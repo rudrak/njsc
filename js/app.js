89352134d4dea901924205f31d76b2499ff7a8d4
Piu Sharma <piu.sharma@oracle.com>
2015-06-20 16:15:09 +0530

    added: changes to input styling, handled minor error with console statements

--- js/app.js
+++ js/app.js
@@ -12,6 +12,10 @@
 			this._options = options;
 			this._elConsole = $(this._options.inputArea);
 			this._eLResult = $(this._options.outputArea);
+			this.commandHistory = {
+				command : [],
+				position: 0
+			};
 
 			var evaluateExpression = function (evaluationString) {
 				var returnExp = undefined;
@@ -20,13 +24,11 @@
 				} catch (err){
 					returnExp = err;
 				}
-
 				return returnExp;
-				
 			},
 
 			writer = function (evalStr) {
-				$(options.outputArea).text(eval(evalStr));
+				return (eval(evalStr));
 			};
 			this.console = {
 				log: writer,
@@ -42,17 +44,35 @@
 					var consoleInput = $(this).text(),
 						consoleOutput = '';
 					//window.console.log(consoleInput);
+					switch(evt.keyCode) {
+						case 13 : 
+							that.commandHistory.command.push(consoleInput);
+							that.commandHistory.position = that.commandHistory.position++;
+							consoleOutput = evaluateExpression(consoleInput);
+							that._eLResult.html(consoleOutput + '<br>' + that._eLResult.html());
+							$(this).text('');
+							break;
+						case 38 : // up arrow
+							if(!that.commandHistory.commad.length && that.commandHistory.position){
+								evaluateExpression(that.commandHistory.command[that.commandHistory.position]);
+								that.commandHistory.position-- ;
+							}
+							break;
+						case 40 : // down arrow
+ 							if(this.commandHistory.position !== this.commandHistory.command.length){
+
+ 							}
+ 							break;
+					}
 					// if the key pressed is enter key then trigger evaluate string
 					if(evt.keyCode === 13){
-						consoleOutput = evaluateExpression(consoleInput);
-						that._eLResult.html(consoleOutput + '<br>' + that._eLResult.html());
-						$(this).text('');
 					}
 				});
 			};
 
 			this.init = function () {
 				this.attachEvents();
+				this._elConsole.focus();
 			}
 		}
 
